2aeb434f39cf92ddc334f61c1ee40f5ecc810dfa
69pmb <pmbroca@gmail.com>
2022-12-28 23:13:12 +0100

    feat: ci

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,33 +1,31 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, waitForAsync } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
 
-import { ButtonComponent } from '../button/button.component';
-import { HomeComponent } from './home.component';
+import { ButtonComponent } from "../button/button.component";
+import { HomeComponent } from "./home.component";
 
-describe('HomeComponent', () => {
+describe("HomeComponent", () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [ButtonComponent, HomeComponent]
-    })
-      .compileComponents();
+      declarations: [ButtonComponent, HomeComponent],
+    }).compileComponents();
   }));
 
-  it('should create', () => {
+  it("should create", () => {
     const fixture = TestBed.createComponent(HomeComponent);
     const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 
-  it('should only create the apps in the list', () => {
+  it("should only create the apps in the list", () => {
     const fixture = TestBed.createComponent(HomeComponent);
     const component = fixture.componentInstance;
     component.apps = [
-      {name: 'Test', route: 'redirect', url: 'test', icon: 'test'}
+      { name: "Test", url: "redirect", color: "adb", icon: "test" },
     ];
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelectorAll('app-button').length).toBe(1);
+    expect(compiled.querySelectorAll("app-button").length).toBe(1);
   });
-
 });
